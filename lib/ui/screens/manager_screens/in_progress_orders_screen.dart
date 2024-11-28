af8805056e09582e3f43f5fffbdcda260a47f3de
WaleedElgamal <waleedelgamall3@gmail.com>
2024-11-28 17:30:55 +0200

    [DEL] removed unused pages

--- lib/ui/screens/manager_screens/in_progress_orders_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class InProgressOrdersScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('In Progress Orders'));
-  }
-}
