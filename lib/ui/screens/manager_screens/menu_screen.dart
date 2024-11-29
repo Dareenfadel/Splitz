74297b530c05d95cbe735ffefd90210debbe9f06
WaleedElgamal <waleedelgamall3@gmail.com>
2024-11-29 15:46:15 +0200

    [MOD] updated management screens for consistency and passing restaurant_id after login

--- lib/ui/screens/manager_screens/menu_screen.dart
+++ lib/ui/screens/manager_screens/menu_screen.dart
@@ -6,6 +6,7 @@ import 'package:splitz/data/services/menu_provider.dart';
 import 'package:splitz/ui/custom_widgets/menu_grid_item.dart';
 import 'package:splitz/ui/custom_widgets/custom_floating_button.dart';
 import 'package:splitz/ui/screens/manager_screens/menu_category_form.dart';
+import '../../../constants/app_colors.dart';
 
 class MenuScreen extends StatelessWidget {
   @override
@@ -22,55 +23,66 @@ class MenuScreen extends StatelessWidget {
             provider.fetchCategories(restaurantId!);
           }
 
-          return Padding(
-            padding: const EdgeInsets.only(top: 30, right: 16, left: 16),
-            child: Stack(children: [
-              Column(children: [
-                // Only show the menu items if they are loaded
-                provider.isLoading
-                    ? Center(child: CircularProgressIndicator())
-                    : provider.categories.isEmpty
-                        ? Center(child: Text('No categories available'))
-                        : Expanded(
-                            child: GridView.builder(
-                              gridDelegate:
-                                  SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 2,
-                                mainAxisSpacing: 24,
-                                crossAxisSpacing: 24,
-                              ),
-                              itemCount: provider.categories.length,
-                              itemBuilder: (context, index) {
-                                MenuCategory category =
-                                    provider.categories[index];
-                                return MenuItem(
-                                  imageUrl: category.image,
-                                  label: category.name,
-                                  onPressed: () {
-                                    print('Clicked on ${category.name}');
+          return Scaffold(
+              appBar: AppBar(
+                title: const Text('Menu'),
+                centerTitle: true,
+                titleTextStyle: const TextStyle(
+                  color: AppColors.textColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              body: Padding(
+                padding: const EdgeInsets.only(top: 30, right: 16, left: 16),
+                child: Stack(children: [
+                  Column(children: [
+                    // Only show the menu items if they are loaded
+                    provider.isLoading
+                        ? Center(child: CircularProgressIndicator())
+                        : provider.categories.isEmpty
+                            ? Center(child: Text('No categories available'))
+                            : Expanded(
+                                child: GridView.builder(
+                                  gridDelegate:
+                                      SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    mainAxisSpacing: 24,
+                                    crossAxisSpacing: 24,
+                                  ),
+                                  itemCount: provider.categories.length,
+                                  itemBuilder: (context, index) {
+                                    MenuCategory category =
+                                        provider.categories[index];
+                                    return MenuItem(
+                                      imageUrl: category.image,
+                                      label: category.name,
+                                      onPressed: () {
+                                        print('Clicked on ${category.name}');
+                                      },
+                                    );
                                   },
-                                );
-                              },
-                            ),
-                          ),
-              ]),
-              user?.userType == "manager"
-                  ? CustomFloatingButton(
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) => AddCategoryDialog(
-                            restaurantId: user!.restaurantId!,
-                            onCategoryAdded: (newCategory) {
-                              provider.addCategory(newCategory, restaurantId!);
-                            },
-                          ),
-                        );
-                      },
-                    )
-                  : SizedBox.shrink(),
-            ]),
-          );
+                                ),
+                              ),
+                  ]),
+                  user?.userType == "manager"
+                      ? CustomFloatingButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => AddCategoryDialog(
+                                restaurantId: user!.restaurantId!,
+                                onCategoryAdded: (newCategory) {
+                                  provider.addCategory(
+                                      newCategory, restaurantId!);
+                                },
+                              ),
+                            );
+                          },
+                        )
+                      : SizedBox.shrink(),
+                ]),
+              ));
         },
       ),
     );
---
+++ lib/ui/screens/manager_screens/orders_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import '../../../constants/app_colors.dart';
+import '../../custom_widgets/orders_list.dart';
+
+class OrdersScreen extends StatefulWidget {
+  final String restaurantId;
+  OrdersScreen({required this.restaurantId});
+
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  final List<String> orderStates = ['Pending', 'In Progress', 'Served'];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(0),
+          child: TabBar(
+            controller: _tabController,
+            tabs: orderStates.map((state) => Tab(text: state)).toList(),
+            labelStyle: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: AppColors.textColor,
+            ),
+            unselectedLabelStyle: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+                color: AppColors.textColor),
+            indicatorColor: Colors.white,
+            indicator: UnderlineTabIndicator(
+                borderSide: const BorderSide(width: 5.0, color: Colors.white),
+                insets: const EdgeInsets.symmetric(horizontal: 16.0),
+                borderRadius: BorderRadius.circular(5)),
+            indicatorSize: TabBarIndicatorSize.tab,
+          ),
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: orderStates.map((status) {
+          return OrdersList(
+            orderStatus: status,
+            restaurantId: widget.restaurantId,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
