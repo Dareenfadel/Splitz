e566f5d148de67189af0b46618691cfcc462e776
Mathew Hany <mathew.hanybb@gmail.com>
2024-11-29 22:54:45 +0200

    feat: Add SelectableUserList UI Widget

---
+++ lib/ui/screens/client_screens/order/widgets/selectable_users_list/selectable_users_list_props.dart
@@ -0,0 +1,11 @@
+class SelectableUsersListProps_User {
+  final String id;
+  final String name;
+  final String imageUrl;
+
+  SelectableUsersListProps_User({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+  });
+}
